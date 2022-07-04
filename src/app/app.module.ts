09ce2c6178258354541126f963422fc45a5c93d3
JorgeHTcoding <jht.coding@gmail.com>
2022-07-04 20:57:26 +0200

    fix routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import { SolicitudPacientesComponent } from './pages/solicitud-pacientes/solicit
 import { AdministracionPacientesComponent } from './pages/administracion-pacientes/administracion-pacientes.component';
 import { TarjetaEventosCancelarComponent } from './component/tarjeta-eventos-cancelar/tarjeta-eventos-cancelar.component';
 import { TarjetaEventosAdministrarComponent } from './component/tarjeta-eventos-administrar/tarjeta-eventos-administrar.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -94,7 +95,8 @@ import { TarjetaEventosAdministrarComponent } from './component/tarjeta-eventos-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
