1e388eb203a20d038ba2ce0c04e3ccb8ae553caf
JorgeHTcoding <jht.coding@gmail.com>
2022-07-13 23:16:43 +0200

    mods chat

--- src/app/pages/landing-paciente/landing-paciente.component.ts
+++ src/app/pages/landing-paciente/landing-paciente.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LandingPacienteComponent implements OnInit {
 
   constructor() { }
+  modalChat:boolean=false;
   show:boolean= true;
  
   ngOnInit(): void {
