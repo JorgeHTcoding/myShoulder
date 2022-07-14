59b816403edf91441d1cf84d5388e0944a208736
JorgeHTcoding <jht.coding@gmail.com>
2022-07-14 19:55:26 +0200

    front login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+// import { UserService } from 'src/app/shared/user.service';
+import { RegistroService } from 'src/app/shared/registro.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user:User;
+
+  constructor(public registroService:RegistroService, private router:Router) { 
+    this.user = new User("","","","","",null,"","","","","","","",)
+   
+  }
+  public onSubmit(form:NgForm){
+    this.registroService.login(this.user).subscribe((data:any)=>{
+      if(data.error==true){
+        this.registroService.logueado = false;
+        this.registroService.user = null;        
+      }else{
+        this.registroService.logueado = true;         
+        this.registroService.user = data.result;   
+        console.log( "este es el user: " + this.user.nick  ) 
+        console.log( "este es la id del usuario : " + this.user.id_user )   
+        
+        this.router.navigateByUrl('/')
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
