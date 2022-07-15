f5783ec5d5f6084f9d46b3b9899b1ba3dfba2444
JorgeHTcoding <jht.coding@gmail.com>
2022-07-15 12:54:27 +0200

    fixlogin

--- src/app/shared/registro.service.ts
+++ src/app/shared/registro.service.ts
@@ -16,6 +16,7 @@ export class RegistroService {
 
   constructor(public http: HttpClient) {
     this.logueado = false;
+   
   }
 
     registrar(user:User):Observable<object>{
@@ -27,9 +28,16 @@ export class RegistroService {
 
     login(user:User):Observable<object>{
       console.log(user)
-      console.log(user.id_user + " este es el id_usuario en el service");
-      this.url ="http://localhost:3000/login";      
+      console.log(user.id_user + " este es el id_user en el front");
+      this.url ="http://localhost:3000/login";                  
       return this.http.post(this.url,user)
-    }          
+     
+    }        
+    // getAll(id_user:number):Observable<Object>{
+    //   console.log(id_user + " Esta es la ID de usuario")  
+    //   this.url ="http://localhost:3000/login" ;     
+    //   console.log(id_user + " AQUI ID USUARIO SERVICE")
+    //   return this.http.get(this.url+"?id_user= "+ id_user)
+    // }      
   }
  
