fd48ac017faffa4beefbb77eba5f09db81b58753
PandaaCode95 <alvaro.amairal@gmail.com>
2022-07-15 10:20:35 +0200

    creacion geventos

--- src/app/pages/mis-eventos-paciente/mis-eventos-paciente.component.ts
+++ src/app/pages/mis-eventos-paciente/mis-eventos-paciente.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient} from '@angular/common/http';
 import { Eventos } from 'src/app/models/eventos';
-
+import { GeventosService } from 'src/app/shared/geventos.service';
 
 
 
@@ -12,12 +12,28 @@ import { Eventos } from 'src/app/models/eventos';
   styleUrls: ['./mis-eventos-paciente.component.scss']
 })
 export class MisEventosPacienteComponent implements OnInit {
-
-  constructor() { }
+  constructor(public geventosService:GeventosService) { }
   eventos=[1,2,3]
+  prueba:any;
+  mostrar:boolean=false;
+  array_eventos:Eventos[];
   ngOnInit(): void {
+    console.log("jEntramos al Getall")
+    let a = this.geventosService.getAll().subscribe((data:Eventos[])=>{
+      for(let i=0;i<data.length;i++){
+        this.array_eventos[i]=data[i];
+      
+      }
+      console.log(this.array_eventos)
+    })
   }
-  
+  //  public borrarevento(id_evento:number){
+  //   this.geventosService.getAll().subscribe((data:Eventos[])=>{
+  //     console.log("Se ha eliminado")
+  //     this.mostrar=true;
+  //   }
+  //  }
+ 
 
   
 }
