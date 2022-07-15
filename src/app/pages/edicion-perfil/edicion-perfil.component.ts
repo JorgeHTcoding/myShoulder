0e0e082ff02d0efb91eea3ac729aaf44bb0ca05f
unknown <lolaairon@gmail.com>
2022-07-15 13:09:32 +0200

    cambios

--- src/app/pages/edicion-perfil/edicion-perfil.component.ts
+++ src/app/pages/edicion-perfil/edicion-perfil.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from 'src/app/shared/user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edicion-perfil',
@@ -9,26 +10,35 @@ import { UserService } from 'src/app/shared/user.service';
   styleUrls: ['./edicion-perfil.component.scss']
 })
 export class EdicionPerfilComponent implements OnInit {
-
+  public usuario:User;
   public miUsuario:User[];
-
+  public id_user:number = 6;
   constructor( public userService:UserService  ) { }
 
   afirEdPerfil:boolean=false;
 
-  modificar(nuevaUrl:HTMLInputElement,nuevoNombre:HTMLInputElement,nuevoApellido:HTMLInputElement,nuevoCorreo:HTMLInputElement,nuevaDireccion:HTMLInputElement,nuevoTlf:HTMLInputElement,nuevoTexto:HTMLInputElement){
+  public modificar(nuevoNombre:HTMLInputElement,nuevoApellido:HTMLInputElement,nuevoCorreo:HTMLInputElement,nuevaDireccion:HTMLInputElement,nuevoTlf:HTMLInputElement,nuevoTexto:HTMLInputElement){
 
-    let nuevoUser = new User ("","",nuevoNombre.value,nuevoApellido.value,nuevoCorreo.value,nuevoTlf.valueAsNumber,nuevoTexto.value,nuevaUrl.value,nuevaDireccion.value,"","","","");
+    let nuevoUser = new User ("","",nuevoNombre.value,nuevoApellido.value,nuevoCorreo.value,nuevoTlf.valueAsNumber,nuevoTexto.value,"",nuevaDireccion.value,"","","","");
       this.userService.edit(nuevoUser).subscribe((data:User)=>{
         this.miUsuario.push(data)
       })
+  }
 
-
+  
+  public mostrar(id_user:number){
+    console.log('Entramos en get');
+    this.userService.getOne(id_user).subscribe((data:User)=>{
+      this.usuario = data;
       
-
+    })
+    console.log(this.usuario)
   }
 
   ngOnInit(): void {
+    console.log('Estamos dentro del on init')
+    this.userService.getOne(this.id_user).subscribe((data:User)=>{
+    this.usuario = data;
+  })
   }
-
 }
