5ad55eca6b565cca0ef3d515c0fd8d018625f371
clau <claudia_diez@outlook.com>
2022-07-16 10:06:16 +0200

    getOneGevent

--- src/app/shared/geventos.service.ts
+++ src/app/shared/geventos.service.ts
@@ -18,16 +18,31 @@ export class GeventosService {
     this.url="http://localhost:3000/eventos"
     return this.http.get(this.url)
   }
-  modificar(id_evento:number):any{
-    id_evento=id_evento;
+
+  modificar(newGevento:Gevento):Observable<Object>{
+    // id_evento=id_evento;
     console.log("Entramos a modificar")
-    this.url="http://localhost:3000/gestion-evento"
-    return this.http.put(this.url,id_evento)
+    this.url="http://localhost:3000/gestion-evento?id="
+    return this.http.put(this.url,newGevento)
   }
   
-  eliminar(id_evento:number):any{
-    console.log("Esntramos a eliminar")
+  eliminar(id_evento:number):Observable<Object>{
+    console.log("Entramos a eliminar")
     this.url="http://localhost:3000/gestion-evento?id="+id_evento
     return this.http.delete(this.url)
   }
+
+  getOne(id_evento:number):Observable<Object>{ 
+    this.url="http://localhost:3000/gestion-evento"
+    console.log("Entramos a getOne")
+    return this.http.get(this.url + "?id_evento=" + id_evento);
+  }
+
+  
+  // getOne(id_evento:number):any{
+  //   console.log("Entramos a getOne")
+  //   this.url="http://localhost:3000/eventos?id="+id_evento
+  //   return this.http.get(this.url)
+  // }
+
 }
