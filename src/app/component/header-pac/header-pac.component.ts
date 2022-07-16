3b168a869684349d0cd761e93e93481bb47e782b
unknown <lolaairon@gmail.com>
2022-07-16 19:41:27 +0200

    ramitita

--- src/app/component/header-pac/header-pac.component.ts
+++ src/app/component/header-pac/header-pac.component.ts
@@ -1,16 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { UserService } from 'src/app/shared/user.service';
-
-=======
 import { UserService } from 'src/app/shared/user.service';
 import { RegistroService } from 'src/app/shared/registro.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { User } from 'src/app/models/user';
->>>>>>> geventos
 @Component({
   selector: 'app-header-pac',
   templateUrl: './header-pac.component.html',
@@ -18,11 +11,7 @@ import { User } from 'src/app/models/user';
 })
 export class HeaderPacComponent implements OnInit {
 
-<<<<<<< HEAD
-  constructor(public userService : UserService , private router:Router ) { }
-=======
   constructor(public registroService:RegistroService, public userService:UserService, private router:Router) { }
->>>>>>> geventos
 
   goEditarPaciente(){
     console.log('Hemos entrado en navigate')
