b7330ba400cdd5704003e3c72c571a796f668d6a
PandaaCode95 <alvaro.amairal@gmail.com>
2022-07-16 18:18:59 +0200

    Apuntar y desapuntar

--- src/app/component/detalle-evento/detalle-evento.component.ts
+++ src/app/component/detalle-evento/detalle-evento.component.ts
@@ -14,14 +14,19 @@ import { Eventos } from 'src/app/models/eventos';
 export class DetalleEventoComponent implements OnInit {
   modalNoIr:boolean=false;
   modalIr:boolean=false;
-  constructor(public geventosService:GeventosService, public apuntadoService:ApuntadoService) { }
-    id:number=20;
-    evento:Gevento;
-    apuntado:Apuntado;
+  user:User;
+  id:number;
+  evento:Gevento;
+  apuntado:Apuntado;
+  constructor(public geventosService:GeventosService, public userService:UserService,public apuntadoService:ApuntadoService) {
+    this.user=this.userService.user;
+    this.id=this.userService.user.id_user
+   }
+    
     
   ngOnInit(): void {
      console.log("Inicializamos la página")
-    this.geventosService.getOne(this.id).subscribe((data:Gevento)=>{
+      this.geventosService.getOne(this.user.id_user).subscribe((data:Gevento)=>{
       console.log("data es:"+ data)
       this.evento=data[0]
       console.log(this.evento)
@@ -29,8 +34,18 @@ export class DetalleEventoComponent implements OnInit {
     console.log("esta es la info")
     console.log(this.evento.titulo + "hehe")
   }
-  public apuntame(id:number,evento:Eventos){
-      // let persona:Apuntado= new Apuntado(id,evento.id_evento)
-      // this.apuntadoService.apuntar()
+  public apuntame(){
+    
+      let persona:Apuntado= new Apuntado(this.evento.id_eventos,this.id)
+      this.apuntadoService.apuntar(persona).subscribe((data:any)=>{
+      console.log("data es:"+ data)
+    })
+      }
+  public desapuntame(){
+    
+      let persona:Apuntado= new Apuntado(this.evento.id_eventos,this.id)
+      this.apuntadoService.desapuntar(persona).subscribe((data:any)=>{
+      console.log("data es:"+ data)
+    })
       }
 }
--- src/app/shared/apuntado.service.ts
+++ src/app/shared/apuntado.service.ts
@@ -7,15 +7,29 @@ import { Observable, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class ApuntadoService {
-  private url:string;
-  public id_usuario:number=1;
-  public id_evento:number=19;
-  public apuntado:Apuntado= new Apuntado(this.id_usuario,this.id_evento)
-  constructor(private http: HttpClient) { }
+  public url:string;
+  // public id_usuario:number=1;
+  // public id_evento:number=19;
+  public apuntado:Apuntado
+  constructor(public http: HttpClient) { }
 
 
-  apuntar(apuntado){
+  apuntar(apuntado:Apuntado):Observable<Object>{
+
     console.log("Entrmos a apuntarEventos")
-    this.url="http://localhost:3000/eventos"
+    this.url="http://localhost:3000/apuntar"
+    console.log("eEnviamos apuntasion")
+    console.log(this.url)
+    console.log(apuntado)
+      return this.http.post<Apuntado>(this.url,apuntado)
+  }
+  desapuntar(apuntado:Apuntado):Observable<Object>{
+    let ap=apuntado
+    console.log("Entrmos a apuntarEventos")
+    this.url="http://localhost:3000/apuntar?id="+apuntado.id_user+"&id_eventos="+apuntado.id_eventos
+    console.log("eEnviamos desapuntasion")
+    console.log(this.url)
+    console.log(apuntado)
+      return this.http.delete<Apuntado>(this.url)
   }
 }
